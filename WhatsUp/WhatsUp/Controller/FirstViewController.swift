1a7d35952bc7cf226630f0fa3ed1b9b967a3e5f7
mohamdan <engineerm86@gmail.com>
2023-07-19 15:10:50 +0300

    preparing view controllers

---
+++ WhatsUp/WhatsUp/Controller/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsUp
+//
+//  Created by mohamdan on 19/07/2023.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
