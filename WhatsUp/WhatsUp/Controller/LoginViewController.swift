f7e787b0b62d7956de762dc53229f0cee2c709e2
mohamdan <engineerm86@gmail.com>
2023-07-20 13:03:37 +0300

    use firestore auth to signin user

--- WhatsUp/WhatsUp/Controller/LoginViewController.swift
+++ WhatsUp/WhatsUp/Controller/LoginViewController.swift
@@ -13,22 +13,28 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
-
+    
     // MARK: - Navigation
-
+    
     @IBAction func loginPressed(_ sender: UIButton) {
         let email = emailTextField.text!
         let password = passwordTextField.text!
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
-          guard let strongSelf = self else { return }
-          // ...
+            guard let strongSelf = self else { return }
+            if error == nil {
+                print("user succesd")
+            }else {
+                print(error.debugDescription)
+                self!.errorLabel.text = error!.localizedDescription
+            }
         }
+        
     }
-    
 }
